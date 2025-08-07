a2d3c9fefa2dc73d672ee718da8091a359aca022
DIDimov24 <DIDimov24@codingburgas.bg>
2025-08-07 16:08:26 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Cat Simulator
+
+This is a cat simulator website, where you can interact with Mr. William II
+
+<img src="./assets/pictures/logo.jpg" style="width: 400px; heigth: 400px; margin-block: 1rem;">
+
+[![Live Preview](https://img.shields.io/badge/Live%20Preview-Visit%20Site-blue?style=for-the-badge&logo=google-chrome&logoColor=white)](https://your-live-site-url.com)
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The website](#the-website)
+  - [Screenshots](#screenshots)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The website
+
+The user can pet William, but he also should be careful not to annoy him!
+
+### Screenshots
+
+![Home page](./assets/screenshots/home.png)
+![Cat in a happy mood](./assets/screenshots/happy-mood.png)
+
+## Acknowledgments
+
+Mr. William II was designed by [JYStoykova24](https://github.com/JYStoykova24)
---
+++ script/app.js
@@ -0,0 +1,116 @@
+const catBtn = document.querySelector('.cat-btn');
+const cat = document.querySelector('.cat-picture');
+
+const scratchContainer = document.querySelector('.scratch-container');
+const scratch = document.querySelector('.scratch');
+
+let petCount = 0;
+
+function touchCat() {
+    if (isCatAngry()) {
+        return;
+    }
+
+    petCount++;
+
+    if (petCount === 10) {
+        setWarning();
+    }
+
+    if (petCount === 20) {
+        annoyCat();
+        return;
+    }
+
+    petCat();
+};
+
+const petCat = () => {
+    playMeowSound();
+
+    setHappyState();
+
+    setTimeout(() => {
+        removeHappyState();
+    }, 500);
+};
+
+const annoyCat = () => {
+    petCount = 0;
+
+    setAngryState();
+    playAngrySound();
+    setShakeAnimation();
+    setScratch();
+    playScratchSound();
+    setScratchAnimation();
+
+    setTimeout(() => {
+        removeAngryState();
+    }, 1500);
+
+    setTimeout(() => {
+        removeShakeAnimation();
+        removeScratch();
+        removeScratchAnimation();
+    }, 2500);
+};
+
+const setNormalState = () => cat.classList.add('normal-state');
+const setHappyState = () => cat.classList.add('happy-state');
+const setAngryState = () => cat.classList.add('angry-state');
+
+const removeNormalState = () => cat.classList.remove('normal-state');
+const removeHappyState = () => cat.classList.remove('happy-state');
+const removeAngryState = () => cat.classList.remove('angry-state');
+
+const setShakeAnimation = () => catBtn.classList.add('shake-animation');
+const removeShakeAnimation = () => catBtn.classList.remove('shake-animation');
+
+const setScratch = () => scratchContainer.classList.remove('hidden');
+const removeScratch = () => scratchContainer.classList.add('hidden');
+
+const setScratchAnimation = () => {
+    scratchContainer.classList.add('scratch-animation');
+    scratchContainer.classList.add('scratch-animation');
+};
+const removeScratchAnimation = () => {
+    scratchContainer.classList.remove('scratch-animation');
+    scratchContainer.classList.remove('scratch-animation');
+};
+
+const isCatAngry = () => cat.classList.contains('angry-state');
+
+const setWarning = () => {
+    const warning = document.querySelector('.warning');
+
+    warning.classList.remove('hidden');
+
+    setTimeout(() => {
+        warning.classList.add('hidden');
+    }, 3000);
+};
+
+const playMeowSound = () => {
+    const meowSound = new Audio('../assets/sounds/meow.mp3');
+
+    meowSound.currentTime = 0;
+    meowSound.volume = 0.1;
+    meowSound.play();
+};
+
+const playAngrySound = () => {
+    const angrySound = new Audio('../assets/sounds/angry.mp3');
+
+    angrySound.currentTime = 0;
+    angrySound.volume = 0.2;
+    angrySound.play();
+};
+
+const playScratchSound = () => {
+    const scratchSound = new Audio('../assets/sounds/scratch.mp3');
+
+    scratchSound.currentTime = 0;
+    scratchSound.volume = 0.4;
+    scratchSound.play();
+};
